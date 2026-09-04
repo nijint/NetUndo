6eaf9b2b66fa200699ff86e9a962555b6c1b51e3
nijint <tnijin123@gmail.com>
2026-09-04 20:53:55 +0530

    style: black and grey dark map theme with zero API key watermarks

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -123,8 +123,9 @@ const MapContainer = ({ selectedNetwork, isReportingMode, onMapClick, onConfirmP
         zoomControl={false}
       >
         <TileLayer
+          className="dark-tiles"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         <MapController
